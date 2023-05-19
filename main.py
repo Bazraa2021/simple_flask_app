bd2a983afab12b8de9fc86b6ac23a58d9ed99fb4
bazraa <bazraa1007@yahoo.com>
2023-05-18 23:03:52 -0500

    added new files

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Bazraa'
+
+
+@app.route('/form', methods=['GET', 'POST'])
+def render_form():
+    message = ''
+    if request.method == 'POST':
+        text = request.form.get('text')
+        message = text.lower()
+    return render_template('form.html', message=message)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
